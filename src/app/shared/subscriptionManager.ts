dbe4be49412cc9fdc00b17721e1c69f49cf14db3
Ighor Maia <ighormaia@gmail.com>
2021-07-02 00:14:28 -0300

    removing unused stuff

--- src/app/shared/subscriptionManager.ts
+++ src/app/shared/subscriptionManager.ts
@@ -31,31 +31,4 @@ export class SubscriptionManager<T> {
       this.subscriptions = [];
     }
   }
-
-  public removeById(id: number): boolean {
-    for (let i = 0; i < this.subscriptions.length; i++) {
-      const element = this.subscriptions[i];
-
-      if (element.id === id) {
-        (element.subscription as any).unsubscribe();
-        this.subscriptions.splice(i, 1);
-        return true;
-      }
-    }
-    return false;
-  }
-
-  public removeByTag(tag: string): boolean {
-    let removed = false;
-    for (let i = 0; i < this.subscriptions.length; i++) {
-      const element = this.subscriptions[i];
-
-      if (element.tag === tag) {
-        (element.subscription as any).unsubscribe();
-        this.subscriptions.splice(i, 1);
-        removed = true;
-      }
-    }
-    return removed;
-  }
 }
